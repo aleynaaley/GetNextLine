57904abe1ca881133a110366ca76bd103b63e78a
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-02-07 21:37:10 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alerkul <alerkul@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 17:02:23 by alerkul           #+#    #+#             */
+/*   Updated: 2024/02/07 21:21:31 by alerkul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// read , malloc, free kullanabilirsin
+//fd nin işaret ettiği metin dosyasının her seferinde sadece tek satır okuyacak 
+
+
+#include get_next_line.h
+
+
+char *get_next_line(int fd){
+
+}
+
