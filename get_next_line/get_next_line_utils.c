b8320a118a60a60f0ae28a10a166c1360267ba6d
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-02-09 23:21:49 +0300

    Update get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -11,5 +11,29 @@
 /* ************************************************************************** */
 
 
-#include get_next_line.h
+#include "get_next_line.h"
+
+char	*ft_read_to_left_str(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
 
