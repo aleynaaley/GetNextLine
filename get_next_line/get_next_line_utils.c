46f905f3491ba330d2f2276f049ef676d165ecb7
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-02-10 19:14:51 +0300

    Update get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -10,30 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "get_next_line.h"
 
-char	*ft_read_to_left_str(int fd, char *left_str)
-{
-	char	*buff;
-	int		rd_bytes;
-
-	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buff)
+// buffer size kadar satır üzerinde okuma yapayıyro ve /n 
+char *read_line(int fd, static char new_str){
+	char	*ytr;
+	int		byte_count;
+	
+	ytr = malloc((BUFFER_SİZE + 1)* sizeof(char));
+	if(!ytr)
 		return (NULL);
-	rd_bytes = 1;
-	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	byte_count = 1;       //okunan byte sayısı
+	while(!ft_strchr(new_str, '\n') && byte_count != 0)
 	{
-		rd_bytes = read(fd, buff, BUFFER_SIZE);
-		if (rd_bytes == -1)
+		byte_count = read(fd, ytr, BUFFER_SIZE);
+		if(byte_count == -1)
 		{
-			free(buff);
+			free(ytr);
 			return (NULL);
 		}
-		buff[rd_bytes] = '\0';
-		left_str = ft_strjoin(left_str, buff);
+		ytr[byte_count] = '\0';
+		new_str = ft_strjoin(new_str, ytr);
 	}
-	free(buff);
-	return (left_str);
+	free(ytr);
+	return(new_str);	
 }
 
+
+// bu fonksiyonda newstr de ki alt satırda okuduğu fazlalığı çıkarıp str ye ilk satırı atar
+char	*get_str(char *new_str)
+{
+
+	
+}
+
+
+char	*newstr(char new_str)
+{
+
+	
+}
+
+
+char	*ft_strchr(char *s, int c)
+{
+	while (*s && *s != (char)c)
+		s++;
+	if (*s == (char)c)
+		return ((char *)s);
+	return (0);
+}
