c88464775064cc107304e1f2cfa2a8ad4fe8e283
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-02-12 15:59:05 +0300

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -11,20 +11,20 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <unistd.h>
-#include <fcntl.h>
-
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 char	*ft_strchr(char *s, int c);
-char	*newstr(char new_str);
+char	*ft_new_str(char *new_str);
 char	*get_str(char *new_str);
-char	*read_line(int fd,char *new_str);
-
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
 #endif
